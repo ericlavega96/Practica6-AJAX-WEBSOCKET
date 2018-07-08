13ee1cae27a9570be5ab08e304b116fa09c8135b
Adonis Veloz <20140452@ce.pucmm.edu.do>
2018-07-08 13:19:06 -0400

    subiendo clase de manejador de mensajes.

---
+++ src/main/java/websocket/ManejadorDeMensajes.java
@@ -0,0 +1,55 @@
+package websocket;
+
+
+import logical.Main;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
+import java.io.IOException;
+
+public class ManejadorDeMensajes {
+    /**
+     * Una vez conectado el cliente se activa este metodo.
+     * @param usuario
+     */
+    @OnWebSocketConnect
+    public void conectando(Session usuario){
+        System.out.println("Conectando Usuario: "+usuario.getLocalAddress().getAddress().toString());
+        Main.usuariosConectados.add(usuario);
+    }
+
+    /**
+     * Una vez cerrado la conexión, es ejecutado el metodo anotado
+     * @param usuario
+     * @param statusCode
+     * @param reason
+     */
+    @OnWebSocketClose
+    public void cerrandoConexion(Session usuario, int statusCode, String reason) {
+        System.out.println("Desconectando el usuario: "+usuario.getLocalAddress().getAddress().toString());
+        Main.usuariosConectados.remove(usuario);
+    }
+
+    /**
+     * Una vez recibido un mensaje es llamado el metodo anotado.
+     * @param usuario
+     * @param message
+     */
+    @OnWebSocketMessage
+    public void recibiendoMensaje(Session usuario, String message) {
+        System.out.println("Recibiendo del cliente: "+usuario.getLocalAddress().getAddress().toString()+" - Mensaje"+message);
+        try {
+            //Enviar un simple mensaje al cliente que mando al servidor..
+            usuario.getRemote().sendString("Mensaje enviado al Servidor: "+message);
+            //mostrando a todos los clientes
+            Main.enviarMensajeAClientesConectados(message, "azul");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
